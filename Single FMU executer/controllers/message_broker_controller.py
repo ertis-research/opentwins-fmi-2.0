43740cc1b6e3dfcdce6a171702f5129feb045256
sergioinf <sergioip@uma.es>
2024-10-01 02:27:35 +0000

    Optional port and changed sending forrmat

--- Single FMU executer/controllers/message_broker_controller.py
+++ Single FMU executer/controllers/message_broker_controller.py
@@ -6,7 +6,7 @@ class MessageBrokerController:
     def __init__(self):
         # Target broker information
         self.BROKER_IP       = os.getenv('BROKER_IP')
-        self.BROKER_PORT     = int(os.getenv('BROKER_PORT'))
+        self.BROKER_PORT     = int(os.getenv('BROKER_PORT')) if os.getenv('BROKER_PORT') != None else None
         self.BROKER_TOPIC    = os.getenv('BROKER_TOPIC')
         self.BROKER_USERNAME = os.getenv('BROKER_USERNAME')
         self.BROKER_PASSWORD = os.getenv('BROKER_PASSWORD')
@@ -15,7 +15,10 @@ class MessageBrokerController:
         self.client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, "fmi-simulator-"+os.getenv('SIMULATION_ID'))
         if self.BROKER_USERNAME != None and self.BROKER_PASSWORD != None:
             self.client.username_pw_set(username=self.BROKER_USERNAME, password=self.BROKER_PASSWORD)
-        self.client.connect(self.BROKER_IP, self.BROKER_PORT)
+        if self.BROKER_PORT != None:
+            self.client.connect(self.BROKER_IP, self.BROKER_PORT)
+        else:
+            self.client.connect(self.BROKER_IP)
 
     def send_message(self, messages):
         self.client.publish(self.BROKER_TOPIC, messages)
